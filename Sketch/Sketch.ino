69f94fc667e93eecdece35167e49e4e1818353fb
speed-str <aamirshakir98@gmail.com>
2017-09-08 13:04:04 +0500

    Arduino Sketch
    Uploaded the receiving end code for Arduino Uno

---
+++ Sketch/Sketch.ino
@@ -0,0 +1,23 @@
+int incomingByte = 0;
+
+void setup() {
+  // put your setup code here, to run once:
+  Serial.begin(115200);
+  Serial.println("Initialized \n");
+}
+
+void loop() {
+
+  while (Serial.available() > 0)
+    {
+        if (Serial.available() > 0) {
+                // read the incoming byte:
+                incomingByte = Serial.read();
+
+                // say what you got:
+                //Serial.print("I received: ");
+                //Serial.println(incomingByte);
+                PORTD = byte(incomingByte);
+         }
+    }
+}
