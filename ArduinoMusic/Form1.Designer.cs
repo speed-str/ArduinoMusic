e11ba245d72f583d0e20ff78a6b58640efd4c322
speed-str <aamirshakir98@gmail.com>
2017-09-08 13:02:15 +0500

    Add files via upload

---
+++ ArduinoMusic/Form1.Designer.cs
@@ -0,0 +1,152 @@
+﻿namespace ArduinoMusic
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.serialPort1 = new System.IO.Ports.SerialPort(this.components);
+            this.btnSend = new System.Windows.Forms.Button();
+            this.cmbBaud = new System.Windows.Forms.ComboBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.btnOpen = new System.Windows.Forms.Button();
+            this.cmbPorts = new System.Windows.Forms.ComboBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.btnImport = new System.Windows.Forms.Button();
+            this.btnReceive = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // btnSend
+            // 
+            this.btnSend.Location = new System.Drawing.Point(28, 71);
+            this.btnSend.Name = "btnSend";
+            this.btnSend.Size = new System.Drawing.Size(230, 49);
+            this.btnSend.TabIndex = 0;
+            this.btnSend.Text = "Send!";
+            this.btnSend.UseVisualStyleBackColor = true;
+            this.btnSend.Click += new System.EventHandler(this.btnSend_Click);
+            // 
+            // cmbBaud
+            // 
+            this.cmbBaud.FormattingEnabled = true;
+            this.cmbBaud.Location = new System.Drawing.Point(66, 42);
+            this.cmbBaud.Name = "cmbBaud";
+            this.cmbBaud.Size = new System.Drawing.Size(59, 21);
+            this.cmbBaud.TabIndex = 1;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(28, 19);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(32, 13);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Port: ";
+            // 
+            // btnOpen
+            // 
+            this.btnOpen.Location = new System.Drawing.Point(179, 16);
+            this.btnOpen.Name = "btnOpen";
+            this.btnOpen.Size = new System.Drawing.Size(79, 23);
+            this.btnOpen.TabIndex = 3;
+            this.btnOpen.Text = "Open";
+            this.btnOpen.UseVisualStyleBackColor = true;
+            this.btnOpen.Click += new System.EventHandler(this.btnOpen_Click);
+            // 
+            // cmbPorts
+            // 
+            this.cmbPorts.FormattingEnabled = true;
+            this.cmbPorts.Location = new System.Drawing.Point(66, 16);
+            this.cmbPorts.Name = "cmbPorts";
+            this.cmbPorts.Size = new System.Drawing.Size(59, 21);
+            this.cmbPorts.TabIndex = 1;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(28, 45);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(38, 13);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Baud: ";
+            // 
+            // btnImport
+            // 
+            this.btnImport.Location = new System.Drawing.Point(179, 42);
+            this.btnImport.Name = "btnImport";
+            this.btnImport.Size = new System.Drawing.Size(79, 23);
+            this.btnImport.TabIndex = 3;
+            this.btnImport.Text = "Import Wav";
+            this.btnImport.UseVisualStyleBackColor = true;
+            this.btnImport.Click += new System.EventHandler(this.btnImport_Click);
+            // 
+            // btnReceive
+            // 
+            this.btnReceive.Location = new System.Drawing.Point(28, 126);
+            this.btnReceive.Name = "btnReceive";
+            this.btnReceive.Size = new System.Drawing.Size(230, 41);
+            this.btnReceive.TabIndex = 0;
+            this.btnReceive.Text = "Receive";
+            this.btnReceive.UseVisualStyleBackColor = true;
+            this.btnReceive.Click += new System.EventHandler(this.btnReceive_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(277, 179);
+            this.Controls.Add(this.btnImport);
+            this.Controls.Add(this.btnOpen);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.cmbPorts);
+            this.Controls.Add(this.cmbBaud);
+            this.Controls.Add(this.btnReceive);
+            this.Controls.Add(this.btnSend);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.Form1_FormClosing);
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.IO.Ports.SerialPort serialPort1;
+        private System.Windows.Forms.Button btnSend;
+        private System.Windows.Forms.ComboBox cmbBaud;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button btnOpen;
+        private System.Windows.Forms.ComboBox cmbPorts;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button btnImport;
+        private System.Windows.Forms.Button btnReceive;
+    }
+}
+
---
+++ ArduinoMusic/Form1.cs
@@ -0,0 +1,99 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.IO.Ports;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ArduinoMusic
+{
+    public partial class Form1 : Form
+    {
+        byte[] HexBytes;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnSend_Click(object sender, EventArgs e)
+        {
+            if (HexBytes != null)
+            {
+                serialPort1.Write(HexBytes, 0, HexBytes.Length);
+            }
+        }
+
+        private void btnReceive_Click(object sender, EventArgs e)
+        {
+            if (serialPort1.IsOpen == true)
+            {
+                string msg = serialPort1.ReadExisting();
+                MessageBox.Show(msg);
+            }
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            string[] ports = SerialPort.GetPortNames();
+            string[] baud = { "4800", "9600", "19200", "38400", "57600", "115200", "230400" , "250000" };
+            cmbPorts.Items.AddRange(ports);
+            cmbBaud.Items.AddRange(baud);
+            cmbPorts.SelectedIndex = 0;
+            cmbBaud.SelectedIndex = 0;
+            btnSend.Enabled = false;
+
+        }
+
+        private void btnOpen_Click(object sender, EventArgs e)
+        {
+            btnOpen.Enabled = false;
+            try
+            {
+                serialPort1.PortName = cmbPorts.Text;
+                serialPort1.BaudRate = Convert.ToInt32(cmbBaud.Text);
+                serialPort1.Open();
+                btnSend.Enabled = true;
+            }
+            catch (Exception)
+            {
+                throw;
+            }
+        }
+
+        private void btnImport_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog openFileDialog1 = new OpenFileDialog();
+            openFileDialog1.InitialDirectory = "d:\\";
+            openFileDialog1.Filter = "Wav files (*.wav)|*.wav|All files (*.*)|*.*";
+            openFileDialog1.FilterIndex = 2;
+            openFileDialog1.RestoreDirectory = true;
+
+            if (openFileDialog1.ShowDialog() == DialogResult.OK)
+            {
+                try
+                {
+                    string directory = openFileDialog1.FileName;
+                    HexBytes = File.ReadAllBytes(directory);
+
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show("Error: Could not read file from disk. Original error: " + ex.Message);
+                }
+            }
+        }
+
+        private void Form1_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            if (serialPort1.IsOpen == true)
+            {
+                serialPort1.Close();
+            }
+        }
+    }
+}
